46ca2ef1790a29057e801ba22be4c4a0208ecdb0
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-10-11 18:09:40 +0000

    : - Increasing Triplet Subsequence - Runtime - 156 ms (5.23%), Memory - 61.6 MB (100.00%)

---
+++ 0334-increasing-triplet-subsequence/solution_2022-10-11_18-09-40.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        int a,b;
+        a=b= INT_MAX;
+        for(auto &e:nums){
+            if(e<= a) a =e;
+            else if(e<= b) b = e;
+            else return true;
+        }
+        return false;
+    }
+};
