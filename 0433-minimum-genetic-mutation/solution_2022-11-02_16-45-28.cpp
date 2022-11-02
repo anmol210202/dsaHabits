950a111b0d8504ece8b31524ed0c4f7f043dd48f
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-02 16:45:28 +0000

    : - Minimum Genetic Mutation - Runtime - 4 ms (0.93%), Memory - 6.8 MB (100.00%)

---
+++ 0433-minimum-genetic-mutation/solution_2022-11-02_16-45-28.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+ queue<string> q;
+        unordered_map<string,int> vis;
+        int steps=0;
+        q.push(start);
+        vis[start]=1;
+        while(!q.empty()){
+            for(int i=q.size(); i>0; i--){
+                auto sq=q.front(); q.pop();
+                if(sq==end) return steps;
+                for(int j=0; j<8; j++){
+                    char temp = sq[j];
+                    for(int k=0; k<4; k++){
+                        sq[j]="ACGT"[k];
+                        if(!vis[sq] and find(bank.begin(),bank.end(),sq)!=bank.end()){
+                            q.push(sq);
+                            vis[sq] = 1;
+                        }
+                    }
+                    sq[j]=temp;
+                }   
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
