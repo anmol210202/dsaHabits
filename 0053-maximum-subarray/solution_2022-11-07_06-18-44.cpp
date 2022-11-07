c1883a905a3c6e381a1998e5dbde1931bb45505a
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-07 06:18:44 +0000

    : - Maximum Subarray - Runtime - 220 ms (5.06%), Memory - 67.8 MB (99.98%)

---
+++ 0053-maximum-subarray/solution_2022-11-07_06-18-44.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int DAC(vector<int>& nums , int s,int e){
+        if(s>e) return INT_MIN;
+        int ls= 0, rs=0;
+        int mid=(s+e)/2;
+        for(int i=mid-1, cs=0; i>=s; i--){
+            cs+=nums[i];
+            ls=max(cs,ls);
+        }
+        for(int i=mid+1, cs=0; i<=e;i++ ){
+            cs+=nums[i];
+            rs=max(cs,rs);
+        }
+        return max({DAC(nums,s,mid-1),DAC(nums,mid+1,e),ls+rs+nums[mid]});
+    }
+    
+    int maxSubArray(vector<int>& nums) {
+        return DAC(nums,0,size(nums)-1);
+    }
+};
+
+
