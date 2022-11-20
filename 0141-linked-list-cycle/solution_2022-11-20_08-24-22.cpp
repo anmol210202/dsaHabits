6c5d0620009b205b7d46b54a49eadc74bc81a353
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-20 08:24:22 +0000

    : - Linked List Cycle - Runtime - 31 ms (5.32%), Memory - 13.8 MB (14.49%)

---
+++ 0141-linked-list-cycle/solution_2022-11-20_08-24-22.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<ListNode*,int> m ;
+    bool hasCycle(ListNode *head) {
+        if(head==NULL) return false;
+        if(m.find(head)!=m.end()) return true;
+        else m[head]++;
+        return hasCycle(head->next);
+    }
+};
