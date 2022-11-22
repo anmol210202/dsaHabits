7b7c06f3c531eaffcdd69ced8373f955a7706394
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-22 16:06:20 +0000

    : - Perfect Squares - Runtime - 4 ms (95.72%), Memory - 6 MB (100.00%)

---
+++ 0279-perfect-squares/solution_2022-11-22_16-06-20.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int x = sqrt(n);
+        if (x * x == n) return 1;
+        while (n % 4 == 0) n /= 4;
+        if (n % 8 == 7) return 4;
+        for (int i = 1; i * i <= n; i++) {
+            int j = sqrt(n - i * i);
+            if (i * i + j * j == n) return 2;
+        }
+        return 3;
+    }
+};
