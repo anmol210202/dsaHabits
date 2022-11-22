acb12b76ac49145a5453211ded1d9d239b1d13fb
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-22 12:36:57 +0000

    : - Palindrome Number - Runtime - 35 ms (5.10%), Memory - 5.9 MB (100.00%)

---
+++ 0009-palindrome-number/solution_2022-11-22_12-36-57.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int reverse(int x){
+        int rev = 0;
+        while(x > 0){
+            int pop = x % 10;
+            x /= 10;
+            if(rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return false;
+            if(rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return false;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+    
+    bool isPalindrome(int x) {
+        return x >= 0 && x == reverse(x);
+    }
+};
