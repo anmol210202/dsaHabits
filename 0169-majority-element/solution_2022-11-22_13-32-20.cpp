6f945d8111b0fa1823f44b975dcfc9541048e705
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-11-22 13:32:20 +0000

    : - Majority Element - Runtime - 52 ms (6.77%), Memory - 19.6 MB (99.87%)

---
+++ 0169-majority-element/solution_2022-11-22_13-32-20.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        for(auto it = m.begin(); it != m.end(); it++) 
+            if(it->second >nums.size()/2) return it->first;
+        return 0;
+    }
+};
