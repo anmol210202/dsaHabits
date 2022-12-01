d8d1f7d52ec77aac21df33e225b73a18e409df1a
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2022-12-01 18:31:11 +0000

    : - Plus One - Runtime - 7 ms (0.14%), Memory - 8.8 MB (100.00%)

---
+++ 0066-plus-one/solution_2022-12-01_18-31-11.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1; i>=0; i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                break;
+            } else{
+                if(i!=0){
+                    digits[i]=0;
+                } else{
+                    digits[i]=0;
+                    digits.insert(digits.begin(),1);
+                    break;
+                }
+            }
+        }
+        return digits;
+    }
+};
