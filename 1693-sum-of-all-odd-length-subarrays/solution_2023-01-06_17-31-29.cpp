5cf8b3b6bf22c2ce0fc44e587d9dbd9a0fd7439b
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-01-06 17:31:29 +0000

    : - Sum of All Odd Length Subarrays - Runtime - 5 ms (6.63%), Memory - 8.3 MB (100.00%)

---
+++ 1693-sum-of-all-odd-length-subarrays/solution_2023-01-06_17-31-29.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int s=arr.size() , a=0,temp=0;
+        for(int i=0; i<s; i++){
+            a+=(arr[i]*ceil((i+1)*(s-i)/2.0));
+        }
+        return a;
+    }
+};
