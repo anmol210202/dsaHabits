8d6b1f6d3176a503b376edadd9bd5c0fe26ba7a9
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-01-09 10:52:12 +0000

    : - Squares of a Sorted Array - Runtime - 43 ms (5.91%), Memory - 26 MB (100.00%)

---
+++ 1019-squares-of-a-sorted-array/solution_2023-01-09_10-52-12.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(auto &e:nums) e=e*e;
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
