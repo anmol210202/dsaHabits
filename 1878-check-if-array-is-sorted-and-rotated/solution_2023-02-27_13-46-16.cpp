ebe1906044e38aa9afc49af6e95a7682c604cc0a
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-02-27 13:46:16 +0000

    : - Check if Array Is Sorted and Rotated - Runtime - 3 ms (3.23%), Memory - 8.3 MB (100.00%)

---
+++ 1878-check-if-array-is-sorted-and-rotated/solution_2023-02-27_13-46-16.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int s=nums[0],n=nums.size(),x=0;
+        for(int i=0; i<n;i++){
+            if(i==n-1){ if(nums[i]>s) x++;}
+            else {if(nums[i]>nums[i+1]) x++;}
+        }
+        // cout<<x<<endl;
+        return x<=1;
+    }
+};
