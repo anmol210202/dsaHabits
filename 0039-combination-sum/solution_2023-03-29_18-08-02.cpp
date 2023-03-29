518558efa9df3c368595bc8e7205b7dbf1adbb9c
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-03-29 18:08:02 +0000

    : - Combination Sum - Runtime - 4 ms (43.46%), Memory - 10.8 MB (100.00%)

---
+++ 0039-combination-sum/solution_2023-03-29_18-08-02.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void r(vector<int> &can ,int i, int t,vector<int> &tmp,vector<vector<int>> &ans){
+        if(t==0) {ans.push_back(tmp);return;}
+        for(int j=i; j<can.size(); j++){
+            if(can[i]>t) continue;
+            tmp.push_back(can[j]);
+            r(can,j,t-can[j],tmp,ans);
+            tmp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int> tmp;
+        vector<vector<int>> ans;
+        r(candidates,0,target,tmp,ans);
+        return ans;
+    }
+};
