be388dbc3f9bc33789aac817a2627f269f665fd7
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-04-01 12:38:24 +0000

    : - Subarrays with K Different Integers - Runtime - 133 ms (24.95%), Memory - 45.1 MB (96.08%)

---
+++ 1034-subarrays-with-k-different-integers/solution_2023-04-01_12-38-24.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int atMost(vector<int> &arr,int k){
+        int l=0,c=0;
+        unordered_map<int,int> m;
+        for(int r=0;r<arr.size();r++){
+            if(!m[arr[r]]++) k--; 
+            while(k<0){
+                if(!--m[arr[l]]) k++;
+                l++;
+            }
+            c+=r-l+1;
+        }
+        return c;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+       return atMost(nums,k)-atMost(nums,k-1);
+    }
+};
