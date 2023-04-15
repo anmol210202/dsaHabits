e2926521c110591740075d1ef3ce0621c811814f
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-04-15 14:02:12 +0000

    : - Validate Stack Sequences - Runtime - 11 ms (3.53%), Memory - 15.4 MB (100.00%)

---
+++ 0983-validate-stack-sequences/solution_2023-04-15_14-02-12.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> s;
+        int i=0;
+        for(auto &e:pushed){
+            s.push(e);
+            while(!s.empty() and s.top()==popped[i]) s.pop(), i++;
+        }
+        return s.empty();
+    }
+};
