a08e4abfcf7a8420c62e22100d55725e50657322
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-08-26 18:11:04 +0000

    : - Two Sum - Runtime - 10 ms (46.02%), Memory - 11.4 MB (99.99%)

---
+++ 0001-two-sum/solution_2023-08-26_18-11-04.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int > m;
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            int cmp = target - nums[i];
+            if(m.find(cmp)!=m.end()) return {m[cmp],i};
+            m[nums[i]]=i;
+        }
+        return {};
+    }
+};
