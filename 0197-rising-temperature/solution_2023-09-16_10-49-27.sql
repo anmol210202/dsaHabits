012e9345378246b8cb5d29d90af3f3ab75b0e235
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-09-16 10:49:27 +0000

    : - Rising Temperature - Runtime - 959 ms (38.88%), Memory - 0.0B (100.00%)

---
+++ 0197-rising-temperature/solution_2023-09-16_10-49-27.sql
@@ -0,0 +1,7 @@
+SELECT w2.id
+# FROM Weather w1 , Weather w2
+# WHERE DATEDIFF(w1.recordDate , w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF (w2.recordDate , w1.recordDate) = 1 and w2.temperature > w1.temperature
