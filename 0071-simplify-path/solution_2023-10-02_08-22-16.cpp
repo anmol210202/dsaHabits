9c2f28394acb9b9fb0e3098c3f70ba9cf317e642
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-02 08:22:16 +0000

    : - Simplify Path - Runtime - 4 ms (36.63%), Memory - 9.7 MB (99.13%)

---
+++ 0071-simplify-path/solution_2023-10-02_08-22-16.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        int n =  path.size();
+        stack<string> s;
+
+        for(int i=0; i<n; i++){
+            if(path[i]=='/') continue;
+
+            string temp;
+
+            while(i<n and path[i]!='/') temp+=path[i++];
+
+            if(temp ==".") continue;
+            else if(temp == "..") {
+                if(!s.empty()) s.pop();
+            } else s.push(temp);
+        }
+
+        path.clear();
+
+        while(!s.empty()){
+            path = '/' + s.top() + path;
+            s.pop();
+        }
+
+        return path.empty() ? "/" : path;
+    }
+};
