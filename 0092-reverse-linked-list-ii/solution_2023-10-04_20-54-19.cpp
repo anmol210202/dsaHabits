5c58226ded25effd513094d68be2f584efc76fb4
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-04 20:54:19 +0000

    : - Reverse Linked List II - Runtime - 4 ms (0.36%), Memory - 7.8 MB (100.00%)

---
+++ 0092-reverse-linked-list-ii/solution_2023-10-04_20-54-19.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* zeroNode = new ListNode();
+        ListNode* prev = zeroNode;
+        prev->next = head;
+        
+        int c=0;
+
+        while(c<left-1){
+            prev=prev->next;
+            c++;
+        }
+
+        ListNode * curr = prev->next;
+        c=0;
+        while(c<right-left){
+            ListNode* nxt = curr->next;
+            curr->next = nxt->next;
+            nxt->next = prev->next;
+            prev->next = nxt;
+            c++;
+        }
+        
+        return zeroNode->next;
+    }
+};
