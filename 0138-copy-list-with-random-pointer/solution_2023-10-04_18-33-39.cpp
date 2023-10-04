381ecd79be5e3979112d2ea82fcd7d8a472227c6
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-04 18:33:39 +0000

    : - Copy List with Random Pointer - Runtime - 6 ms (56.92%), Memory - 11.8 MB (100.00%)

---
+++ 0138-copy-list-with-random-pointer/solution_2023-10-04_18-33-39.cpp
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* curr = head;
+        if(head == NULL) return NULL;
+
+        while(curr!=NULL){
+            Node* tmp = new Node(curr->val);
+            tmp->next = curr->next;
+            curr->next = tmp;
+            curr = curr->next->next;
+        }
+
+        curr = head;
+        
+
+        while(curr!=NULL){
+             if(curr->random){
+                curr->next->random = curr->random->next;
+                cout<<curr->random->next->val<<endl;
+             }
+             curr = curr->next->next;
+        }
+
+        curr = head;
+        Node* res = head->next;
+        Node* tmp = head->next;
+
+        while(curr!=NULL){
+            curr->next = tmp->next;
+            curr= curr->next;
+            if(tmp!=NULL and curr!=NULL )tmp->next = curr->next;
+            tmp = tmp->next;
+        }
+
+        return res;
+
+    }
+};
