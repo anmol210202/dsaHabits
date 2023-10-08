98b660bbac97c3db381c0d92e36e4a190800a6fd
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-08 10:33:38 +0000

    : - Symmetric Tree - Runtime - 6 ms (0.92%), Memory - 16.6 MB (99.95%)

---
+++ 0101-symmetric-tree/solution_2023-10-08_10-33-38.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool check(TreeNode* left , TreeNode* right){
+        if(!left and !right) return true;
+        if(!left or !right or left->val != right->val) return false;
+        return check(left->left,right->right) and check(left->right,right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return check(root->left , root->right);
+        
+    }
+};
