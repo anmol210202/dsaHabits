d647cd985cb688a66394474967b577f4d7ac8066
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-10 19:49:10 +0000

    : - Sum Root to Leaf Numbers - Runtime - 0 ms (100.00%), Memory - 9.4 MB (100.00%)

---
+++ 0129-sum-root-to-leaf-numbers/solution_2023-10-10_19-49-10.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+
+    void traverse(TreeNode* root , int num){
+        if(!root->left and !root->right){
+            ans+=num*10+root->val;
+            return ;
+        }
+
+        if(root->left){
+            traverse(root->left , num*10 + root->val);
+        }
+        if(root->right){
+            traverse(root->right , num*10 + root->val);
+        }
+        return ;
+    }
+    
+    int sumNumbers(TreeNode* root) {
+        if(!root) return ans;
+        traverse(root,0);
+        return ans;
+    }
+};
