21599e7c52f35924a95a98b4fc953a93b3315f83
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2023-10-14 20:04:32 +0000

    : - Clone Graph - Runtime - 0 ms (100.00%), Memory - 9.7 MB (100.00%)

---
+++ 0133-clone-graph/solution_2023-10-14_20-04-32.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    Node* dfs(Node* root , map<Node*,Node*> &m){
+        vector<Node*> neighbor ; 
+        Node* tmp = new Node(root->val);
+        m[root]=tmp;
+        for(auto e:root->neighbors){
+            if(m.find(e)!=m.end()){
+                neighbor.push_back(m[e]);
+            } else {
+                neighbor.push_back(dfs(e,m));
+            }
+        }
+        tmp->neighbors = neighbor;
+        return tmp; 
+    }
+
+    Node* cloneGraph(Node* node) {
+        map<Node* , Node*> m;
+        if(node == NULL) return NULL;
+        return dfs(node , m);
+    }
+};
