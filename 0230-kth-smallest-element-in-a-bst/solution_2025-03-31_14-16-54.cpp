96217e436a33e5c67378e999461e7bb549de137b
itsme_anmol <81642107+anmol210202@users.noreply.github.com>
2025-03-31 14:16:54 +0000

    : - Kth Smallest Element in a BST - Runtime - 0 ms (100.00%), Memory - 24.5 MB (43.09%)

---
+++ 0230-kth-smallest-element-in-a-bst/solution_2025-03-31_14-16-54.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorder(TreeNode* node,int &ans, int &k){
+        if(!node) return ;
+        inorder(node->left,ans,k);
+        if(--k==0){
+        ans = node->val;
+            return ;
+        }
+        inorder(node->right,ans,k);
+        }
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int ans = 0;
+        inorder(root,ans,k);
+        return ans;
+    }
+};
